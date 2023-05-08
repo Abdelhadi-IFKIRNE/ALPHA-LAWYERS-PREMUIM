b9c47033f23b0beadf5738d912b9bec2d25e0dad
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-08 15:59:33 +0000

    update ...

---
+++ src/main/java/com/example/alphalawyerspremuim/AlphaLawyersPremuimApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphalawyerspremuim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaLawyersPremuimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaLawyersPremuimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alphalawyerspremuim/AlphaLawyersPremuimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alphalawyerspremuim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaLawyersPremuimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
